0af67dd64d9a333964f0d1cfa9d52b637be7b326
Yousaf Syed <mmesunny@gmail.com>
2016-01-03 14:13:40 +0000

    Preparing for composer

--- autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include(dirname(__file__).DIRECTORY_SEPARATOR."algorithms".DIRECTORY_SEPARATOR."Search.php");
-include(dirname(__file__).DIRECTORY_SEPARATOR."algorithms".DIRECTORY_SEPARATOR."Sort.php");
-include(dirname(__file__).DIRECTORY_SEPARATOR."containers".DIRECTORY_SEPARATOR."Stack.php");
